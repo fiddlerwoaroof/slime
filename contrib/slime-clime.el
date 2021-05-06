d49269212d4636cf4a035c3fc0e7e5ffe960771f
Luke Gorrie <luke@snabb.solutions>
2021-05-06 10:17:03 +0200

    slime-clime.el: Add workaround to make image map properties reliable

--- contrib/slime-clime.el
+++ contrib/slime-clime.el
@@ -17,24 +17,33 @@
        ;; Stolen mostly from slime-media.el, thanks Christophe!
        (let ((marker (slime-repl-output-target-marker :repl-result)))
          (goto-char marker)
-         (slime-clime-insert-image (copy-seq svg-data) presentations)
+         (slime-clime-insert-image svg-data presentations)
          (insert "\n")
          ;; Move the input-start marker after the REPL result.
          (set-marker marker (point))))
      t)
     ((:accept-for-clime thread tag input-context)
-     (slime-clime-set-input-context input-context))
+     (slime-clime-set-input-context (slime-connection) input-context)
+     ;;(slime-dispatch-event `(:emacs-return ,thread ,tag 2565))
+     t)
     (t nil)))
 
 (defun slime-clime-insert-image (svg-data presentations)
   (let ((map (slime-clime-presentations-map presentations)))
-    (insert-image (slime-clime-create-image (copy-seq svg-data) map))
-    ))
-;;    (put-text-property (char-before) (point) 'slime-clime-connection (slime-connection))
-;;    (put-text-property (char-before) (point) 'slime-clime-presentation-map map)))
+    (insert-image (slime-clime-create-image svg-data map))
+    (put-text-property (char-before) (point)
+                       'slime-clime-connection (slime-connection))
+    (put-text-property (char-before) (point)
+                       'slime-clime-presentation-map map)))
 
 (defun slime-clime-create-image (svg-data map)
-  (create-image svg-data 'svg t :map map))
+  ;; Somehow create-image does not work properly when passed the image
+  ;; data and map attributes directly. This is pure voodoo. Making a
+  ;; fresh copy is the only reliable workaround I have been able to
+  ;; find. -luke
+  (eval (read (let (print-length print-depth)
+                (prin1-to-string `(create-image ,svg-data 'svg t
+                                                :map ',map))))))
 
 (defun slime-clime-presentations-map (presentations)
   "Return an image 'map' property for PRESENTATIONS."
@@ -48,7 +57,17 @@
 
 (defun slime-clime-set-input-context (slime-connection input-context)
   "Set INPUT-CONTEXT for SLIME-CONNECTION.
-The input context is a list of presentation IDs ready for ACCEPT.")
+The input context is a list of presentation IDs ready for ACCEPT."
+  (slime-clime-map-images
+   (lambda (image)
+     (when (eq (get-text-property (point) 'slime-clime-connection)
+               (slime-connection))
+       ;; restrict presentations
+       ))))
+
+;;(defun slime-clime-map-images (fn)
+;;  "Call FN with each CLIME image in all buffers in Emacs."
+
 
 (provide 'slime-clime)
 
