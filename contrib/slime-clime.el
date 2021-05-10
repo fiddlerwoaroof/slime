8b1ea319b9d2522445454ec7b9de2e10180474bc
Luke Gorrie <luke@snabb.solutions>
2021-05-10 17:15:54 +0200

    slime-clime.el: Click presentations to answer ACCEPT requests
    Lisp code can run e.g.
    
      (accept 'string)
    
    and then strings in Emacs presentations can be clicked to accept the
    corresponding object in Lisp.
    
    Rough!
    
    Some nasty hacks to workaround apparent Emacs quirks;
    Allows one ACCEPT per connection but probably should be global;
    Is not yet calling the code to re-enable all presentations;
    Seems to happily accept objects of the wrong type!
    
    But getting there...

--- contrib/slime-clime.el
+++ contrib/slime-clime.el
@@ -10,6 +10,49 @@
   (:on-load
    (add-hook 'slime-event-hooks 'slime-dispatch-clime-event)))
 
+;;;; Keymap
+
+(defvar slime-clime-image-keymap
+  (let ((map (make-sparse-keymap)))
+    ;; Currently using a catch-all keybinding because Emacs seems (?)
+    ;; to insist that a specific [mouse-1] binding would be matched as
+    ;; [area-id mouse-1] i.e. that we would need to define a
+    ;; keybinding for each and every presentation. That's a bit much.
+    ;; So for now we just catch all key/mouse events on the image.
+    ;;
+    ;; (If you put the Emacs point/cursor over the image and the
+    ;; keyboard stops working then try clicking somewhere else so that
+    ;; the point/cursor can escape...
+    ;;(define-key map [mouse-1] 'slime-clime-mouse-1)
+    (define-key map [t] 'slime-clime-input-event)
+    map))
+
+(defun slime-clime-input-event (&optional event)
+  (interactive "e")
+  (when (eq (event-basic-type event) 'mouse-1)
+    (slime-clime-mouse-1 event)))
+
+(defun slime-clime-mouse-1 (&optional event)
+  (interactive "e")
+  (let* ((posn (second event))
+         (image (posn-image posn))
+         (area (posn-area posn)))
+    (when (and (slime-clime-input-context)
+               (eq (slime-connection)
+                   (image-property image 'slime-clime-connection)))
+      (slime-clime-accept area))))
+
+(defun slime-clime-accept (id)
+  (let ((index (cl-etypecase id
+                 (integer id)
+                 ;; HACK: string-trim to workaround mangling that's
+                 ;; happening converting IDs between numbers and
+                 ;; symbols -lg
+                 (symbol (cl-parse-integer (string-trim (symbol-name id)
+                                                        "[^0-9]" "[^0-9]"))))))
+    (cl-destructuring-bind (thread tag ctx) (slime-clime-input-context)
+      (slime-dispatch-event `(:emacs-return ,thread ,tag ,index)))))
+
 (defun slime-dispatch-clime-event (event)
   (slime-dcase event
     ((:write-clime svg-data presentations)
@@ -23,51 +66,87 @@
          (set-marker marker (point))))
      t)
     ((:accept-for-clime thread tag input-context)
-     (slime-clime-set-input-context (slime-connection) input-context)
-     ;;(slime-dispatch-event `(:emacs-return ,thread ,tag 2565))
+     (slime-clime-set-input-context (slime-connection) thread tag input-context)
      t)
     (t nil)))
 
 (defun slime-clime-insert-image (svg-data presentations)
-  (let ((map (slime-clime-presentations-map presentations)))
-    (insert-image (slime-clime-create-image svg-data map))
-;;    (put-text-property (char-before) (point)
-;;                       'slime-clime-connection (slime-connection))
-;;    (put-text-property (char-before) (point)
-;;                       'slime-clime-presentation-map map)
-    ))
+  (let* ((map (slime-clime-presentations-map presentations))
+         (props (list 'slime-clime-presentations presentations
+                      :map map
+                      'slime-clime-connection (slime-connection))))
+    (insert-image (slime-clime-create-image svg-data map props))
+    (put-text-property (1- (point)) (point)
+                       'keymap slime-clime-image-keymap)))
 
-(defun slime-clime-create-image (svg-data map)
+(defun slime-clime-create-image (svg-data map properties)
   ;; Somehow create-image does not work properly when passed the image
   ;; data and map attributes directly. This is pure voodoo. Making a
   ;; fresh copy is the only reliable workaround I have been able to
   ;; find. -luke
-  (eval (read (let (print-length print-depth)
-                (prin1-to-string `(create-image ,svg-data 'svg t
-                                                :map ',map))))))
+  (eval (append (read (let (print-length print-depth)
+                        (prin1-to-string `(create-image ,svg-data 'svg t :map ',map))))
+                (mapcar (lambda (prop) (list 'quote prop)) properties))))
 
 (defun slime-clime-presentations-map (presentations)
   "Return an image 'map' property for PRESENTATIONS."
   (mapcar (lambda (presentation)
-            (cl-destructuring-bind (id area tooltip) presentation
-              `(,area ,(gensym) (pointer arrow help-echo ,tooltip))))
+            (cl-destructuring-bind (number area tooltip) presentation
+              (let ((id (make-symbol (prin1-to-string number))))
+                `(,area ,(make-symbol (prin1-to-string id)) (pointer arrow help-echo ,tooltip)))))
           presentations))
 
 
 ;;;; Input
 
-(defun slime-clime-set-input-context (slime-connection input-context)
+(slime-def-connection-var slime-clime-input-context nil
+  "Current CLIM input context for the connection.
+List of (THREAD TAG INPUT-CONTEXT)")
+
+(defun slime-clime-set-input-context (slime-connection thread tag input-context)
   "Set INPUT-CONTEXT for SLIME-CONNECTION.
 The input context is a list of presentation IDs ready for ACCEPT."
+  ;; If an input context already exists then abort it as stale.
+  (when (slime-clime-input-context)
+    (cl-destructuring-bind (thread tag ctx) (slime-clime-input-context)
+      (slime-dispatch-event `(:emacs-return ,thread ,tag :abort)))
+    (setf (slime-clime-input-context) nil))
+  ;; Save input context
+  (setf (slime-clime-input-context)
+        (list thread tag input-context))
+  ;; Update presentations
   (slime-clime-map-images
    (lambda (image)
      (when (eq (get-text-property (point) 'slime-clime-connection)
                (slime-connection))
-       ;; restrict presentations
-       ))))
+       (let* ((image (get-text-property (point) 'display))
+              (presentations (image-property image 'slime-clime-presentations)))
+         ;; Activate only the areas representing presentations
+         ;; acceptable in the input context.
+         (setf (image-property image :map)
+               (cl-remove-if-not (lambda (area)
+                                   (member (second area)
+                                           (cdr input-context)))
+                                 presentations)))))))
 
-;;(defun slime-clime-map-images (fn)
-;;  "Call FN with each CLIME image in all buffers in Emacs."
+;;; XXX unused
+(defun slime-clime-reset-input-context (connection)
+  "Reset the current input context."
+  (slime-clime-map-images
+   (lambda (image)
+     (setf (image-property image :map)
+           (image-property image 'slime-clime-presentations)))))
+
+(defun slime-clime-map-images (fn)
+  "Call FN with each CLIME image in all buffers in Emacs."
+  (dolist (b (buffer-list))
+    (with-current-buffer b
+      (save-excursion
+        (goto-char (point-min))
+        (while (text-property-search-forward 'slime-clime-connection)
+          (let ((image (get-text-property (point) 'display)))
+            (when image
+              (funcall fn image))))))))
 
 
 ;;;; Image information
